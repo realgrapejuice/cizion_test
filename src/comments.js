bb6c303a668e1a05fa58bf182d96bb32d11bcd0d
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-05-28 15:33:24 +0900

    feat: 좋아요, 싫어요, 공유하기 버튼을 클릭할 때 카운트가 올라갈 수 있도록 구현함 2021.05.28:15:31 좋아요, 싫어요, 공유하기 버튼 클릭 시 카운트가 올라가도록 구현. (해당 기능은 상태를 받고, 상태가 true일 경우 해당 카운트를 1 증가시키는 방식으로 접근할 필요가 있어 보임)

--- src/comments.js
+++ src/comments.js
@@ -5,6 +5,7 @@ import { comments } from "./db.js";
 export default class Comment {
   constructor() {
     this.app = document.querySelector(".app");
+    this.index = 0;
     this.commentSection = document.createElement("section");
     this.commentSection.setAttribute("class", "comment__container");
     this.db = comments;
@@ -21,6 +22,7 @@ export default class Comment {
     const shares = document.createElement("button");
 
     div.setAttribute("class", "userComment");
+    div.setAttribute("id", `tag${this.index++}`);
     id.setAttribute("class", "userComment__name");
     postedAt.setAttribute("class", "userComment__postedAt");
     post.setAttribute("class", "userComment__post");
@@ -42,12 +44,72 @@ export default class Comment {
     this.app.append(this.commentSection);
   }
 
+  _findIndex(e) {
+    const currentTarget = e.target;
+    let index;
+    if (
+      currentTarget.className == "userComment__comment" ||
+      currentTarget.className == "userComment__likes" ||
+      currentTarget.className == "userComment__dislikes" ||
+      currentTarget.className == "userComment__shares"
+    ) {
+      index = currentTarget.parentNode.id;
+    } else return;
+    return index;
+  }
+
+  _updateCount(e, tag, index) {
+    const currentTarget = e.target;
+    const comment = document.querySelector(
+      `#tag${index} > .userComment__comment`
+    );
+    const likes = document.querySelector(`#tag${index} > .userComment__likes`);
+    const dislikes = document.querySelector(
+      `#tag${index} > .userComment__dislikes`
+    );
+    const shares = document.querySelector(
+      `#tag${index} > .userComment__shares`
+    );
+    if (currentTarget && tag) {
+      switch (currentTarget.className) {
+        case "userComment__comment":
+          ++tag.count.comments;
+          comment.innerText = tag.count.comments;
+          break;
+        case "userComment__likes":
+          ++tag.count.likes;
+          likes.innerText = tag.count.likes;
+          break;
+        case "userComment__dislikes":
+          ++tag.count.dislikes;
+          dislikes.innerText = tag.count.dislikes;
+          break;
+        case "userComment__shares":
+          ++tag.count.share;
+          shares.innerText = tag.count.share;
+          break;
+        default:
+          return;
+      }
+    }
+  }
+
   loadComment() {
     const db = localStorage.getItem("db");
     if (db) {
+      // 로딩 후 데이터를 화면에 보여주는 것을 담당
       const parsedDb = JSON.parse(db);
       parsedDb.forEach((comment) => this._visualize(comment));
-    } else {
+      this._updateCount(parsedDb);
+      this.commentSection.addEventListener("click", (e) => {
+        const index =
+          this._findIndex(e) && Number(this._findIndex(e).split("g")[1]);
+        const currentTag = parsedDb[index];
+        this._updateCount(e, currentTag, index);
+      });
+    }
+    // 초기화면에서 데이터를 불러온 뒤 업데이트 시겨주는 것을 담당
+    else {
       localStorage.setItem("db", JSON.stringify(this.db));
       this.db.forEach((comment) => this._visualize(comment));
     }
